b89113b3fc14376035e2bd0229189d8573acfe44
Joeun <imjoeunha@gmail.com>
2018-06-27 11:07:43 +0900

    update: main.js
    - 구현2

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const { SHA256 }  = require('crypto-js');
-const block_chain = {};
 const go = (seed, ...fns) => fns.reduce((res, f) => f(res), seed);
 const tap = fn => arg => (fn(arg), arg);
+
+let G = typeof window == 'object' ? window : global;
+
+G.CHAIN = {};
+G.DIFF = 2;
+G.HEAD = '';
+G.USERS = {
+  '0001': { name: 'A', balance: 70 },
+  '0002': { name: 'B', balance: 50 },
+  '0003': { name: 'C', balance: 110 }
+};
 
 function mining(previousHash, timestamp, transactions, diff) {
   let block = { previousHash, timestamp, transactions, nonce: 0 };
   block.hash = calculate_hash(block);
-  
+
   while (block.hash.substring(0, diff) !== Array(diff + 1).join('0')) {
     block.nonce++;
     block.hash = calculate_hash(block);
@@ -20,26 +30,52 @@ function calculate_hash(block) {
   return SHA256(previousHash + timestamp + JSON.stringify(transactions) + nonce).toString();
 }
 
-var d = 2, prev_hash = '';
-
 function add_block(block) {
-  // 검증 절차를 거친 뒤 아래 활동을 해야함
-  // 채굴에 성공한 노드가 보상을 받아감
-  block_chain[block.hash] = block;
-  prev_hash = block.hash;
-  return block_chain;
+  if (!is_block_valid(block)) return false;
+  G.CHAIN[block.hash] = block;
+  G.HEAD = block.hash;
+  return true;
 }
 
-console.log('\n\n< 체인 시작 />')
+function is_block_valid(block) {
+  if (block.hash.substring(0, G.DIFF) !== Array(G.DIFF + 1).join('0')) return false;
+  return calculate_hash(block) === block.hash;
+}
+
+function reward_to(address) {
+  return function(is_success) {
+    if (is_success) {
+     G.USERS[address].balance += G.DIFF * 10;
+     G.DIFF++;
+    }
+  }
+}
+
+function is_chain_valid(chain) {
+  return Object.keys(chain).every(hash => {
+    const current = chain[hash];
+    const previous = chain[current.previousHash];
+    if (!previous) return true; // Genesis Block
+    if (hash !== calculate_hash(current)) return false;
+    if (current.previousHash !== calculate_hash(previous)) return false;
+    return true;
+  });
+}
+
+const MY_ADDRESS = '0001';
+
+console.log('\n\n< 체인 시작 />');
 !function recur(trx) {
   console.log("\n\n=== 채굴 시작 ===");
   return go(
-    mining(prev_hash, new Date(), trx, d++),
+    mining(G.HEAD, new Date(), trx, G.DIFF),
     tap(console.log),
     add_block,
-    function(chain) {
-      console.log(`=== 채굴 성공: 블록 길이 => ${Object.keys(chain).length} ===`)
-      return d < 5 ? recur(trx) : console.log('\n\n< 체인 종료 />');
+    tap(reward_to(MY_ADDRESS)),
+    function(is_success) {
+      let chain = G.CHAIN;
+      console.log(`=== 채굴 ${is_success ? '성공' : '실패'}: 블록 길이 => ${Object.keys(chain).length} ===`);
+      return G.DIFF < 5 ? recur(trx) : console.log(`\n== 현재 잔액 ${G.USERS[MY_ADDRESS].balance} | 체인 유효성 ${is_chain_valid(chain)} ==\n< 체인 종료 />`);
     }
   )
-}([{ a: 10, b: -10 }, { a: -10, b: 20, c: -10 }])
+}([{ A: -10, B: 10 }]);
